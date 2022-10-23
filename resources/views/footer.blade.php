a43613460aba5eaea67fa0ac263448ccac093073
TajulMunandar <89116213+TajulMunandar@users.noreply.github.com>
2022-10-23 18:14:42 +0700

    Profile Company

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,4 @@
+
+    <div class="copyright">
+        <p>Copyright © 2021 Cinema Vision. All Rights Reserved.</p>
+    </div>
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,19 @@
+<section class="header">
+        <header>
+            <h1><a href="beranda.php">CINEMA<span>VISION </span></a></h1>
+            <ul>
+                <li><a href="/" class="" aria-current="page">Merchandise</a></li>
+
+                <li><a href="/animation" class="" aria-current="page">Animation</a></li>
+
+                <li><a href="/cinema" class="" aria-current="page">Cinematography</a></li>
+
+                <li><a href="/about" class="" aria-current="page">About</a></li>
+            </ul>
+        </header>
+            <div class="text">
+                <h2>Let's Keep Your Moment With Us</h2>
+                <p>Sudut Rantau is a website that sells Merchandise, Photography, Videography, and Animation services</p>
+                <a href="">Visit Us To Know More</a>
+            </div>
+    </section>
